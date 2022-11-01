6e6ea0bcdd95e6545182fe081d93611b70188e32
SohWeeKiat <27757880+SohWeeKiat@users.noreply.github.com>
2022-11-01 23:18:49 +0800

    [!]Grouped user & staff ui into different functions

--- src/Boundary/GoldenVillage.java
+++ src/Boundary/GoldenVillage.java
@@ -23,140 +23,28 @@ public class GoldenVillage {
 		CineplexUI cineplexUI = new CineplexUI(sc,cinemaUI);
 		// Displaying the main menu
 		do {
-			System.out.println("\n========== Main Menu ==========\n");
-			System.out.println("Please select user type:");
-			System.out.println("1. Cinema Staff");
-			System.out.println("2. Movie Goer");
-			System.out.println("3. Exit");
-			System.out.println("\n===============================\n");
-
-			// Getting user's choice, then deciding which options to show
+			userSelectorUI.DisplayUserTypeSelection();
 			choice = sc.nextInt();
-			sc.nextLine(); // To remove the "enter"
-			switch (choice) {
-
-			// If cinema staff
-			case 1: {
-				String userName, passWord;
-
-				// Need to login first
-				boolean Success = userSelectorUI.HandleCinemaStaffLogin();
-				do {
-					// If username and password match, then show the actions
-					// Correct username = "USERNAME1", correct password = "PASSWORD123"
-					if (Success) {
-						System.out.println("\n========== Cinema Staff ==========\n");
-						System.out.println("Please select what you want to do:");
-						System.out.println("1. Create/Update/Remove movie listing");
-						System.out.println("2. Create/Update/Remove cinema showtimes and the movies to be shown");
-						System.out.println("3. Configure system settings");
-						System.out.println("4. Exit");
-						System.out.println("\n==================================\n");
-						choice = sc.nextInt();
-
-						// Getting user's choice, then deciding which options to show
-						switch (choice) {
-						case 1: {
-							movieUI.HandleMovieUI();
-							break;
-						}
-
-						case 2: {
-							cineplexUI.HandleCineplexUI();
-							break;
-						}
-
-						case 3: {
-
-							break;
-						}
-
-						// Exiting
-						case 4: {
-							System.out.println("Exiting...");
-							return;
-						}
-						}
+			sc.nextLine();      // To remove the "enter"
+			switch(choice) {
+				case 1:
+					if (!userSelectorUI.HandleCinemaStaffLogin()) {
+						System.out.println("You have entered incorrect username / password, please rerun the program again");
+						choice = 3;//force exit
+					}else{
+						userSelectorUI.DisplayStaffActions(movieUI, cineplexUI);
 					}
-
-					// Else if the details are wrong, return
-					else {
-						System.out.println(
-								"You have entered incorrect username / password, please rerun the program again");
-						return;
-					}
-				} while (choice != 4);
-
-				break;
-			}
-
-			// If movie goer
-			case 2: {
-				User u = userSelectorUI.HandleMovieGoer();
-				System.out.println("\n========== Movie Goer ==========\n");
-				System.out.println("Please select what you want to do:");
-				System.out.println("1. Search movies");
-				System.out.println("2. List movies");
-				System.out.println("3. View movies details");
-				System.out.println("4. Purchase movie tickets"); // Further branch to seats also
-				System.out.println("5. View booking history");
-				System.out.println("6. List top 5 movies by ticket sales");
-				System.out.println("7. List top 5 movies by overall ratings");
-				System.out.println("8. Exit");
-				System.out.println("\n================================\n");
-				choice = sc.nextInt();
-
-				// Getting user's choice, then deciding which options to show
-				switch (choice) {
-				case 1: {
 					break;
-				}
-
-				case 2: {
+				case 2:
+					userSelectorUI.DisplayMovieGoerActions();
 					break;
-				}
-
-				case 3: {
-					break;
-				}
-
-				case 4: {
-					break;
-				}
-
-				case 5: {
-					break;
-				}
-
-				case 6: {
-					break;
-				}
-
-				case 7: {
-					break;
-				}
-
-				// Exiting
-				case 8: {
+				case 3:
 					System.out.println("Exiting...");
-					return;
-				}
-				}
-				break;
+					break;
+				default:
+					System.out.println("Please key in valid number (1 - 3)");
+					break;
 			}
-
-			// If exit
-			case 3: {
-				System.out.println("Exiting...");
-				return;
-			}
-
-			// If user key in invalid number, ask them try again
-			default: {
-				System.out.println("Please key in valid number (1 - 3)");
-			}
-			}
-
-		} while (choice < 1 || choice > 3);
+		} while (choice != 3);
 	}
 }
--- src/Boundary/UserSelectorUI.java
+++ src/Boundary/UserSelectorUI.java
@@ -14,7 +14,7 @@ public class UserSelectorUI {
         this.sc = sc;
     }
 
-    public void DisplayUserSelection()
+    public void DisplayUserTypeSelection()
     {
         System.out.println("\n========== Main Menu ==========\n");
         System.out.println("Please select user type:");
@@ -22,36 +22,6 @@ public class UserSelectorUI {
         System.out.println("2. Movie Goer");
         System.out.println("3. Exit");
         System.out.println("\n===============================\n");
-
-        int choice = sc.nextInt();
-        sc.nextLine();      // To remove the "enter"
-        switch(choice) {
-            case 1:
-                if (!this.HandleCinemaStaffLogin()) {
-                    System.out.println("You have entered incorrect username / password, please rerun the program again");
-                }
-                break;
-            case 2:
-            {
-                User u = this.HandleMovieGoer();
-                break;
-            }
-        }
-    }
-
-    public User HandleMovieGoer()
-    {
-        System.out.println("\n========== Movie Goer ==========\n");
-        System.out.println("Please enter Email:");
-        String email = sc.nextLine();
-        User u = UserManager.getInstance().GetUserByEmail(email);
-        if (u != null)
-            return u;
-        System.out.println("Please enter Name:");
-        String name = sc.nextLine();
-        System.out.println("Please enter phone:");
-        String phone = sc.nextLine();
-        return UserManager.getInstance().AddUser(email, name, phone);
     }
 
     public boolean HandleCinemaStaffLogin()
@@ -67,5 +37,91 @@ public class UserSelectorUI {
         System.out.println("userName = " + userName + ", passWord = " + passWord);
 
         return UserManager.getInstance().Login(userName, passWord);
+    }
+
+    public void DisplayStaffActions(MovieUI movieUI, CineplexUI cineplexUI)
+    {
+        int choice = 0;
+        do {
+            System.out.println("\n========== Cinema Staff ==========\n");
+            System.out.println("Please select what you want to do:");
+            System.out.println("1. Create/Update/Remove movie listing");
+            System.out.println("2. Create/Update/Remove cinema showtimes and the movies to be shown");
+            System.out.println("3. Configure system settings");
+            System.out.println("4. Exit");
+            System.out.println("\n==================================\n");
+
+            choice = sc.nextInt();
+            // Getting user's choice, then deciding which options to show
+            switch (choice) {
+                case 1:
+                    movieUI.HandleMovieUI();
+                    break;
+                case 2:
+                    cineplexUI.HandleCineplexUI();
+                    break;
+                case 3:
+                    break;
+                // Exiting
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+            }
+        }while(choice != 4);
+    }
+
+    private User GetMovieGoerInfo()
+    {
+        System.out.println("\n========== Movie Goer ==========\n");
+        System.out.println("Please enter Email:");
+        String email = sc.nextLine();
+        User u = UserManager.getInstance().GetUserByEmail(email);
+        if (u != null)
+            return u;
+        System.out.println("Please enter Name:");
+        String name = sc.nextLine();
+        System.out.println("Please enter phone:");
+        String phone = sc.nextLine();
+        return UserManager.getInstance().AddUser(email, name, phone);
+    }
+
+
+    public void DisplayMovieGoerActions()
+    {
+        User u = this.GetMovieGoerInfo();
+        System.out.println("\n========== Movie Goer ==========\n");
+        System.out.println("Please select what you want to do:");
+        System.out.println("1. Search movies");
+        System.out.println("2. List movies");
+        System.out.println("3. View movies details");
+        System.out.println("4. Purchase movie tickets"); // Further branch to seats also
+        System.out.println("5. View booking history");
+        System.out.println("6. List top 5 movies by ticket sales");
+        System.out.println("7. List top 5 movies by overall ratings");
+        System.out.println("8. Exit");
+        System.out.println("\n================================\n");
+
+        int choice = sc.nextInt();
+        // Getting user's choice, then deciding which options to show
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            // Exiting
+            case 8:
+                System.out.println("Exiting...");
+                break;
+        }while(choice != 8);
     }
 }
