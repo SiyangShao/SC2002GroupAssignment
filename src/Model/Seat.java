0d102305e87ee32af89f8b4a5d564c9f41858085
Shao Siyang <siyang.shao@outlook.com>
2022-11-11 14:50:05 +0800

    Java doc comment

--- src/Model/Seat.java
+++ src/Model/Seat.java
@@ -3,38 +3,66 @@ package Model;
 import java.io.Serializable;
 
 public class Seat implements Serializable {
+    /**
+     * The Seat ID of the seat
+     */
     protected int seatNo;
+    /**
+     * The Movie ID of the seat
+     */
     protected int MovieID;
 
 //    protected double basePrice;
 
+    /**
+     * The constructor of the seat
+     *
+     * @param seatNo  The seat ID of the seat
+     * @param MovieID The Movie ID of the seat
+     */
     public Seat(int seatNo, int MovieID) {
         setSeatNo(seatNo);
         setMovieID(MovieID);
 //        setBasePrice(basePrice);
     }
 
+    /**
+     * Get the seat number of the seat
+     *
+     * @return The seat number of the seat
+     */
+
     public int getSeatNo() {
         return seatNo;
     }
+
+    /**
+     * Set the seat number of the seat
+     *
+     * @param seatNo The seat number of the seat
+     */
 
     public void setSeatNo(int seatNo) {
         this.seatNo = seatNo;
     }
 
+    /**
+     * Get the Movie ID of the seat
+     *
+     * @return The Movie ID of the seat
+     */
     public int getMovieID() {
         return MovieID;
     }
+
+    /**
+     * Set the Movie ID of the seat
+     *
+     * @param movieID The Movie ID of the seat
+     */
 
     public void setMovieID(int movieID) {
         MovieID = movieID;
     }
 
-//    public double getBasePrice() {
-//        return basePrice;
-//    }
-
-//    public void setBasePrice(double basePrice) {
-//        this.basePrice = basePrice;
-//    }
 }
--- src/Model/Ticket.java
+++ src/Model/Ticket.java
@@ -4,21 +4,42 @@ import java.io.Serializable;
 
 public class Ticket extends Seat implements Serializable {
     //    private int MovieID;
+    /**
+     * The type of the ticket
+     */
     private TicketType type;
+    /**
+     * The price of the ticket
+     */
     private double finalPrice;
-
+    /**
+     * The special price for senior
+     */
     private static double senior = 0.8;
+    /**
+     * The special price for student
+     */
     private static double student = 0.9;
+    /**
+     * The special price for child
+     */
     private static double child = 0.5;
+    /**
+     * The transaction ID of the ticket
+     */
     private final String transactionID;
     //private userid
 
-    public static void changeDiscount(double new_senior, double new_student, double new_child) {
-        senior = new_senior;
-        student = new_student;
-        child = new_child;
-    }
 
+    /**
+     * The constructor of the ticket
+     *
+     * @param seatNo        The seat number of the ticket
+     * @param MovieID       The Movie ID of the ticket
+     * @param basePrice     The base price of the ticket
+     * @param type          The type of the ticket
+     * @param transactionID The transaction ID of the ticket
+     */
     public Ticket(int seatNo, int MovieID, double basePrice, TicketType type, String transactionID) {
         super(seatNo, MovieID);
         this.type = type;
@@ -31,30 +52,80 @@ public class Ticket extends Seat implements Serializable {
         }
     }
 
+    /**
+     * Change the discount for senior
+     *
+     * @param new_senior  The new discount for senior
+     * @param new_student The new discount for student
+     * @param new_child   The new discount for child
+     */
+    public static void changeDiscount(double new_senior, double new_student, double new_child) {
+        senior = new_senior;
+        student = new_student;
+        child = new_child;
+    }
+
+    /**
+     * Get the type of the ticket
+     *
+     * @return The type of the ticket
+     */
 
     public TicketType getType() {
         return type;
     }
 
+    /**
+     * Set the type for ticket
+     *
+     * @param type The type of the ticket
+     */
+
     public void setType(TicketType type) {
         this.type = type;
     }
 
+    /**
+     * Get the final price of the ticket
+     *
+     * @return The final price of the ticket
+     */
     public double getFinalPrice() {
         return finalPrice;
     }
 
+    /**
+     * Set the final price of the ticket
+     *
+     * @param finalPrice The final price of the ticket
+     */
     public void setFinalPrice(double finalPrice) {
         this.finalPrice = finalPrice;
     }
 
+    /**
+     * Get the MovieID of the ticket
+     *
+     * @return The MovieID of the ticket
+     */
     public int getMovieID() {
         return MovieID;
     }
 
+    /**
+     * Set the MovieID of the ticket
+     *
+     * @param movieID  The MovieID of the ticket
+     */
     public void setMovieID(int movieID) {
         MovieID = movieID;
     }
+
+    /**
+     * Get the transaction ID of the ticket
+     *
+     * @return The transaction ID of the ticket
+     */
 
     public String getTransactionID() {
         return transactionID;
